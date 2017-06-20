218e5194180021dd6e4506427ed2a20fe91ee4f6
Fredy B <tovar_fredy@hotmail.com>
2017-06-19 22:13:31 -0500

    middle

--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 
 const reducer = (state, action ) => {
 	/*if (action.type === "ADD_TO_CART"){
@@ -25,4 +25,11 @@ const reducer = (state, action ) => {
 	return state;
 };
 
-export default createStore(reducer, { cart: [] })
+const logger = store => next => action => {
+  console.log('dispatching', action);
+  let result = next(action);
+  console.log('next state', store.getState());
+  return result
+}
+
+export default createStore(reducer, { cart: [] }, applyMiddleware(logger))
